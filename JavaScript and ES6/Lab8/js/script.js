68648f44219c47e01af2c1d48acc11d4a3a81331
Hager Abg El Galil <Hager.galil66@gmail.com>
2023-02-13 22:04:54 +0200

    JavaScript and ES6 Lab8 Update

--- JavaScript and ES6/Lab8/js/script.js
+++ JavaScript and ES6/Lab8/js/script.js
@@ -1,72 +1,165 @@
 var form = document.getElementById("form");
 var newTask = document.getElementById("input-newTask");
-var addBtn = document.getElementById("add-btn");
-var newTaskContainer = document.getElementById("newTask-container");
+var task = document.querySelector(".list");
+var done = document.getElementsByClassName("done");
 
-var completeBtn = [];
-var removeBtn = [];
-var taskName = [];
-var task = [];
+var arrayOfTasks = [];   //Array of Tasks
+
+//Check If There are Tasks in Local Storage
+if(localStorage.getItem("Tasks"))
+{
+    arrayOfTasks = JSON.parse(localStorage.getItem("Tasks"));
+}
+
+getTaskFromLocalStorage();
 
 //Add New Task
-form.addEventListener('submit',function(event){
+form.onsubmit = function(event){
+
     event.preventDefault();
+    addTaskToArray(newTask.value);  
+    newTask.value = ""; //Empty input Field
 
-    var newElem = document.createElement("div");
-    newTaskContainer.appendChild(newElem);
-    newElem.classList.add('list','row', 'justify-content-between', 'col-12', 'py-3', 'rounded-2','my-3');
-    newElem.style.background = "white";
+};
 
-    var name = document.createElement("div");
-    newElem.appendChild(name);
-    name.classList.add('col-12', 'col-md-8', 'col-lg-10','fs-4','taskName');
-    name.innerText = newTask.value;
+//Click on Task Remove Button
+task.addEventListener('click',function(elem){
+    if(elem.target.classList.contains("remove")){
 
-    var correct = document.createElement("div");
-    newElem.appendChild(correct);
-    correct.classList.add('col-5','col-sm-4','col-md-2', 'col-lg-1', 'px-sm-4','mt-2', 'mt-md-0');
-    var correctBtn = document.createElement("button");
-    correct.appendChild(correctBtn);
-    correctBtn.setAttribute('type','submit');
-    correctBtn.classList.add('btn','btn-success','rounded-5');
-    correctBtn.innerHTML = "<i class='bi bi-check-lg'></i>";
+        let removeTarget = elem.target.parentElement.parentElement.parentElement;
 
-    var wrong = document.createElement("div");
-    newElem.appendChild(wrong);
-    wrong.classList.add('col-7','col-sm-4','col-md-2', 'col-lg-1','mt-2', 'mt-md-0');
-    var wrongBtn = document.createElement("button");
-    wrong.appendChild(wrongBtn);
-    wrongBtn.setAttribute('type','submit');
-    wrongBtn.classList.add('btn','btn-danger','rounded-5');
-    wrongBtn.innerHTML = "<i class='bi bi-x-lg'></i>";    
+        //Remove Task From Local Storage by ID
+        RemoveTaskFromLocalStorage(removeTarget.getAttribute("task-id"));
+    
+        //Remove Task From Page
+        removeTarget.remove();
+
+    }
 });
 
-//update Tasks Data every 0.5 sec
-setInterval(function()
-{
-    var completeBtn = document.getElementsByClassName("btn-success");
-    var removeBtn = document.getElementsByClassName("btn-danger");
-    var taskName = document.getElementsByClassName("taskName");
-    var task = document.getElementsByClassName("list");
+//Click on Task Complete Button
+task.addEventListener('click',function(elem){
+    if(elem.target.classList.contains("complete")){
+         
+        let completeTarget = elem.target.parentElement.parentElement.parentElement;
 
-    //Completed Task
-    for(let i = 0; i < completeBtn.length; i++)
-    { 
-        completeBtn[i].onclick = function(){
-            taskName[i].style.textDecoration = "line-through"; 
-            task[i].style.background = "#abf7b1";
-        }
+        //Toggle Completed For the Task
+        toggleTaskStatus(completeTarget.getAttribute("task-id"));
+
+        //Toggle Done Class in Page
+        completeTarget.classList.toggle("done");
+    }
+});
+
+//Add Task Data To Array Function
+function addTaskToArray(newTaskName){
+
+    //Task Data
+    const taskData = {
+        id: Date.now(),
+        title: newTaskName,
+        completed: false
+    };
+
+    //push Tasks to Array Of Tasks
+    arrayOfTasks.push(taskData);
+
+    //Add Tasks To Page 
+    addTaskToPage(arrayOfTasks);
+
+    //Add Tasks To Local Storage
+    addTaskToLocalStorage(arrayOfTasks);
 }
-    //Remove Task
-    for (let i = 0; i < removeBtn.length; i++) {
-        removeBtn[i].onclick = function() {
-            newTaskContainer.removeChild(task[i]);
+
+//Add Task To TO DO List Function
+function addTaskToPage(arrayOfTasks){
+
+    //Empty list of Tasks
+    task.innerHTML = "";
+
+    //Looping On Array Of Tasks
+    arrayOfTasks.forEach(elementTask => {
+
+        //Task Div
+        var newElem = document.createElement("div");
+        task.appendChild(newElem);
+        newElem.classList.add('row', 'justify-content-between', 'col-12', 'py-3', 'rounded-2','my-2','mx-2');
+        newElem.style.background = "white";
+        newElem.setAttribute('task-id',elementTask.id);
+
+        //Task Name Div
+        var name = document.createElement("div");
+        newElem.appendChild(name);
+        name.classList.add('col-12','col-md-8', 'col-lg-8','fs-4','taskName');
+        name.innerText = elementTask.title;
+        
+        //Complete Button
+        var Complete = document.createElement("div");
+        newElem.appendChild(Complete);
+        Complete.classList.add('col-1','col-sm-4','col-md-2', 'col-lg-1', 'px-sm-4','mt-2', 'mt-md-0');
+        var CompleteBtn = document.createElement("button");
+        Complete.appendChild(CompleteBtn);
+        CompleteBtn.setAttribute('type','submit');
+        CompleteBtn.classList.add('btn','btn-success','rounded-5');
+        CompleteBtn.innerHTML = "<i class='bi bi-check-lg complete'></i>";
+        
+        //Remove Button
+        var remove = document.createElement("div");
+        newElem.appendChild(remove);
+        remove.classList.add('col-7','col-sm-4','col-md-2', 'col-lg-1','mt-2', 'mt-md-0');
+        var removeBtn = document.createElement("button");
+        remove.appendChild(removeBtn);
+        removeBtn.setAttribute('type','submit');
+        removeBtn.classList.add('btn','btn-danger','rounded-5');
+        removeBtn.innerHTML = "<i class='bi bi-x-lg remove'></i>";
+
+        //Check If Task Completed
+        if(elementTask.completed)
+        {
+            newElem.classList.add('row', 'done', 'justify-content-between', 'col-12', 'py-3', 'rounded-2','my-2','mx-2');
+        }
+    });   
+}
+
+//Add Tasks To Local Storage Function
+function addTaskToLocalStorage(arrayOfTasks){
+
+    window.localStorage.setItem("Tasks",JSON.stringify(arrayOfTasks));
+
+}
+
+//Get Tasks To Local Storage Function
+function getTaskFromLocalStorage(){
+
+    let data = window.localStorage.getItem("Tasks");
+
+    if(data){
+        let tasks = JSON.parse(data);
+        addTaskToPage(tasks);
+    }
+    
+}
+
+//Remove Tasks From Local Storage By ID Function
+function RemoveTaskFromLocalStorage(id){
+
+    arrayOfTasks = arrayOfTasks.filter((task) => task.id != id);
+    addTaskToLocalStorage(arrayOfTasks);
+
+}
+
+//Toggle Task Status Function
+function toggleTaskStatus(id){
+
+    for (let i = 0; i < arrayOfTasks.length ; i++){
+        if(arrayOfTasks[i].id == id){
+            arrayOfTasks[i].completed == false ? (arrayOfTasks[i].completed = true) : (arrayOfTasks[i].completed = false);
         }
     }
-},500);
 
+    addTaskToLocalStorage(arrayOfTasks);
 
-
+}
 
 
 
