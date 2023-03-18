2c2a654d448b1e6c4426372b818f0f2f0d55a377
Hager Abg El Galil <Hager.galil66@gmail.com>
2023-03-18 19:10:10 +0200

    Laravel Framework Labs

--- Laravel Framework/routes/web.php
+++ Laravel Framework/routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {return view('welcome');});
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::get('/posts/edit/{post}', [PostController::class, 'edit'])->name('posts.edit');
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::put('/posts', [PostController::class, 'update'])->name('posts.update');
